9d13f5153202726fe36c7d71c2473d924198f6ec
Bashsoft707 <alausabashir@gmail.com>
2024-03-13 17:38:35 +0100

    footer update

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,13 +3,25 @@ import styled from "styled-components";
 const Section = styled.section`
   background: #2e0e0e;
   padding-top: clamp(2.1875rem, 0.8681rem + 4.2222vw, 4.5625rem);
-  padding-bottom: clamp(2.375rem, 1.4792rem + 4.6667vw, 8.1875rem);
+  padding-bottom: clamp(2.375rem, 1.4792rem + 4.6667vw, 8.1875rem); 
+  color: #ffffff; 
+  font-size: 14px;
 
   .footer-wrapper {
     display: flex;
     align-items: flex-start;
     justify-content: space-around;
-    color: #ffffff;
+    margin-bottom: 20px;
+
+    .download-section {
+      .download-store {
+        margin-bottom: 40px;
+
+        a {
+          margin-right: 10px;
+        }
+      }
+    }
 
     .footer-list {
       text-transform: capitalize;
@@ -27,6 +39,28 @@ const Section = styled.section`
           cursor: pointer;
           color: #ffffff;
         }
+
+        .footer-social-icons {
+          a {
+            padding: 0 0.48rem 0 0;
+          }
+        }
+      }
+    }
+  }
+
+  .foot-note {
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 0;
+
+    p {
+      a {
+        color: #FFFFFF;
+        margin-right: 10px;
+        text-transform: capitalize;
       }
     }
   }
